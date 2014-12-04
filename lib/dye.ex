7c75b70aeae8398feb75e8f464b8531958e10c55
Kabie <kabie3000@gmail.com>
2014-12-05 01:45:09 +0800

    sigils: d, D, u, U

--- lib/dye.ex
+++ lib/dye.ex
@@ -1,2 +1,64 @@
 defmodule Dye do
+  import IO.ANSI
+
+  defmacro sigil_d({:<<>>, _line, [string]}, [fore]) do
+    foreground(fore) <> string <> reset
+  end
+
+  defmacro sigil_d({:<<>>, _line, [string]}, [fore, back]) do
+    foreground(fore) <> background(back) <> string <> reset
+  end
+
+  defmacro sigil_D({:<<>>, _line, [string]}, [fore]) do
+    bright <> foreground(fore) <> string <> reset
+  end
+
+  defmacro sigil_D({:<<>>, _line, [string]}, [fore, back]) do
+    bright <> foreground(fore) <> background(back) <> string <> reset
+  end
+
+  defmacro sigil_u({:<<>>, _line, [string]}, [fore]) do
+    underline <> foreground(fore) <> string <> reset
+  end
+
+  defmacro sigil_u({:<<>>, _line, [string]}, [fore, back]) do
+    underline <> foreground(fore) <> background(back) <> string <> reset
+  end
+
+  defmacro sigil_U({:<<>>, _line, [string]}, [fore]) do
+    underline <> bright <> foreground(fore) <> string <> reset
+  end
+
+  defmacro sigil_U({:<<>>, _line, [string]}, [fore, back]) do
+    underline <> bright <> foreground(fore) <> background(back) <> string <> reset
+  end
+
+  defp foreground(chr) do
+    case chr do
+      ?b -> black
+      ?c -> cyan
+      ?d -> default_color
+      ?g -> green
+      ?m -> magenta
+      ?r -> red
+      ?u -> blue
+      ?w -> white
+      ?y -> yellow
+    end
+  end
+
+  defp background(chr) do
+    case chr do
+      ?b -> black_background
+      ?c -> cyan_background
+      ?d -> default_background
+      ?g -> green_background
+      ?m -> magenta_background
+      ?r -> red_background
+      ?u -> blue_background
+      ?w -> white_background
+      ?y -> yellow_background
+    end
+  end
+
 end
--- test/dye_test.exs
+++ test/dye_test.exs
@@ -1,7 +1,47 @@
 defmodule DyeTest do
   use ExUnit.Case
+  import Dye
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Dyed text" do
+    [
+      ~d"Black text"b,
+      ~D"Bright black text"b,
+      ~u"Black text with underline"b,
+      ~U"Bright black text with underline"b,
+      ~d"Cyan text"c,
+      ~D"Bright cyan text"c,
+      ~u"Cyan text with underline"c,
+      ~U"Bright cyan text with underline"c,
+      ~d"Default text"d,
+      ~D"Bright default text"d,
+      ~u"Default text with underline"d,
+      ~U"Bright default text with underline"d,
+      ~d"Green text"g,
+      ~D"Bright green text"g,
+      ~u"Green text with underline"g,
+      ~U"Bright green text with underline"g,
+      ~d"Magenta text"m,
+      ~D"Bright magenta text"m,
+      ~u"Magenta text with underline"m,
+      ~U"Bright magenta text with underline"m,
+      ~d"Red text"r,
+      ~D"Bright red text"r,
+      ~u"Red text with underline"r,
+      ~U"Bright red text with underline"r,
+      ~d"Blue text"u,
+      ~D"Bright blue text"u,
+      ~u"Blue text with underline"u,
+      ~U"Bright blue text with underline"u,
+      ~d"White text"w,
+      ~D"Bright white text"w,
+      ~u"White text with underline"w,
+      ~U"Bright white text with underline"w,
+      ~d"Yellow text"y,
+      ~D"Bright yellow text"y,
+      ~u"Yellow text with underline"y,
+      ~U"Bright yellow text with underline"y
+    ]
+    |> Enum.each(&IO.puts/1)
+
   end
 end
